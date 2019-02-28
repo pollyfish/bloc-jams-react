ffd29568aed348c86d5588c78d2ddfc18398f3f1
Polly <polly.fisher@gmail.com>
2019-02-27 20:40:33 -0600

    Checkpoint 3 corrected

--- src/components/Landing.js
+++ src/components/Landing.js
@@ -15,6 +15,7 @@ const Landing = () => (
 <h2 className="point-title">Mobile enabled</h2>
 <p className="point-description">Listen to your music on the go. This streaming service is available on all mobile platforms.</p> </div>
   </section>
+  </section>
 );
 
 export default Landing;
