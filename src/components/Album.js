8f8c1162a4342f585ce431a0552aa02b2dfaaea6
Polly <polly.fisher@gmail.com>
2019-03-06 14:53:00 -0600

    Assignment 8 playerbarbuttons completed

--- src/components/Album.js
+++ src/components/Album.js
@@ -53,6 +53,13 @@ class Album extends Component {
       this.setSong(newSong);
       this.play();
   }
+  handleNextClick(){
+    const currentIndex = this.state.album.songs.findIndex(song => this.state.currentSong === song);
+      const newIndex = Math.min(4, currentIndex + 1);
+      const newSong = this.state.album.songs[newIndex];
+      this.setSong(newSong);
+      this.play();
+  }
 
   onHover(index) {
       this.setState({ isHovered: index });
@@ -111,6 +118,7 @@ class Album extends Component {
            currentSong={this.state.currentSong}
            handleSongClick={() => this.handleSongClick(this.state.currentSong)}
            handlePrevClick={() => this.handlePrevClick()}
+           handleNextClick={() => this.handleNextClick()}
          />
   </section>
 );
