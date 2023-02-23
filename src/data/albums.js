c4c0663e59a5909a4bcd9d7743853be910c871fa
Polly <polly.fisher@gmail.com>
2023-02-23 10:36:30 -0600

    update-music-2-23

--- src/data/albums.js
+++ src/data/albums.js
@@ -12,14 +12,14 @@ export default [{
       { title: 'Magenta', duration: '374.22', audioSrc: '/assets/music/magenta.mp3' }
   ]
 }, {
-    title: 'The Telephone',
-    artist: 'Guglielmo Marconi',
-    releaseInfo: '1909 EM',
+    title: 'Scarred',
+    artist: 'Gary Numan',
+    releaseInfo: '2000',
     albumCover: '/assets/images/album_covers/02.jpg',
-    slug: 'the-telephone',
+    slug: 'scarred',
     songs: [
-      { title: 'Blue', duration: '161.71', audioSrc: '/assets/music/blue.mp3' },
-      { title: 'Green', duration: '103.96', audioSrc: '/assets/music/green.mp3' },
+      { title: 'Pure', duration: '253.09', audioSrc: '/assets/music/pure.mp3' },
+      { title: 'Magic', duration: '296.13', audioSrc: '/assets/music/magic.mp3' },
       { title: 'Red', duration: '268.45', audioSrc: '/assets/music/red.mp3' },
       { title: 'Pink', duration: '153.14', audioSrc: '/assets/music/pink.mp3' },
       { title: 'Magenta', duration: '374.22', audioSrc: '/assets/music/magenta.mp3' }
